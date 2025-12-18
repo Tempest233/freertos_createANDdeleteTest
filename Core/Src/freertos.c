1f254ff83c22419e33654c98884068a0d927d4b6
3dB <a1240472394@gmail.com>
2025-12-18 15:41:00 +0800

    code_2

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -314,7 +314,7 @@ void KEY1_Entry(void *pvParameters)
 
 void USART_Receive_Entry(void *pvParameters)
 {
-  vTaskDelay(NULL);
+  vTaskSuspend(NULL);
   UartPacket_t recv_pkg;
   for (;;)
   {
@@ -379,7 +379,7 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
     msg.len = Size;
     memcpy(msg.payload, RxBuffer, Size);
     xQueueSendFromISR(myQueue, &msg, &xHigherPriorityTaskWoken);
-
+    
     
     HAL_UARTEx_ReceiveToIdle_DMA(&huart1, RxBuffer, RX_BUF_SIZE);
     __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
